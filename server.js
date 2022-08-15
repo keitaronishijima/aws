fcf0a981c161911344ca2c028e55c364397bfedc
Keitaro Nishijima <keitaro_nishijima@brown.edu>
2022-08-15 19:47:16 +0900

    fg

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8080
+const express = require("express");
+const passport = require("passport");
+const passportHttp = require("passport-http");
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const record = { username: "aws", password: "candidate" };
 
-app.get('/secret', (req, res) => {
-    res.send('beist')
-})
+passport.use(
+  new passportHttp.DigestStrategy({ qop: "auth" }, (username, cb) => {
+    if (username === record.username) {
+      return cb(null, username, record.password);
+    } else {
+      return cb(null, false);
+    }
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get(
+  "/secret",
+  passport.authenticate("digest", {
+    session: false,
+  }),
+  (req, res) => {
+    res.send("認証されました");
+  }
+);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
