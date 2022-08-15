4c730ce06200ba7b17ea1662c7df316d133eafbd
Keitaro Nishijima <keitaro_nishijima@brown.edu>
2022-08-15 19:36:28 +0900

    f

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app = express()
 
-app.set('host', '52.195.18.17');
-
 app.get('/', function(req, res){
   res.send('Hello node.js');
 });
 
-app.listen(80, '52.195.18.17');
+app.listen(3000);
